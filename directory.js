0396015fc96a491b0344904eb9b146f9eed48460
ucipass <ucipass@gmail.com>
2017-04-18 12:55:14 -0500

    Duplicate Files Tested

--- directory.js
+++ directory.js
@@ -1,6 +1,6 @@
 var logger = require('winston');
 logger.emitErrs = true;
-logger.loggers.add('DIRECTORY', { console: { level: 'debug', label: "WWW", handleExceptions: true, json: false, colorize: true}});
+logger.loggers.add('DIRECTORY', { console: { level: 'debug', label: "DIRECTORY", handleExceptions: true, json: false, colorize: true}});
 var log = logger.loggers.get('DIRECTORY');
 
 var fs = require("fs")
@@ -34,6 +34,7 @@ module.exports = class{
         var final = new Promise((res,rej)=>{resolve=res;reject=rej})
         fs.stat(directory ? directory : this.fpath ,(err,stat)=>{
             if(err) {
+                log.error(err)
                 reject(err)
             }
             else{
@@ -51,7 +52,8 @@ module.exports = class{
         var final = new Promise((res,rej)=>{resolve=res;reject=rej})
         fs.readdir(directory ? directory : this.fpath, function(err, list) {
             if(err){
-                resolve([])
+                log.error("read fn:",err)
+                reject(err)
             }
             else {
                 resolve( list )
@@ -59,16 +61,16 @@ module.exports = class{
         })
         return final
     } 
-	async readfull(){  
-		var dirlist =  [this.fpath]
-		var filelist = []
+	async filelist(fpath){  
+		var dirlist =  fpath ? [fpath] : [this.fpath]
+		var filearr = []
 
 		while (dirlist.length > 0) {
 			var curdir = dirlist.pop()
-			var curlist = await this.read(curdir)
+			var curlist = await this.read(curdir).catch( (err)=> [] ) // Even if there is an error continue reading directory
 			for(var x = 0; x < curlist.length; x++){
 				var entry = path.join(curdir,curlist[x])
-				var stat = await this.stat(entry)
+				var stat = await this.stat(entry).catch( (err)=> null ) // Even if there is an error continue reading directory
 				if (!stat){
 				}
 				else if (stat.isDirectory()){
@@ -79,10 +81,99 @@ module.exports = class{
 					file.size = stat.size
 					file.ctime = stat.ctime
 					file.mtime = stat.mtime
-					filelist.push(file)
+					filearr.push(file)
 				}
 			}	
 		}
-		return filelist
+		return filearr
 	}
+    async dupFiles(files,sortby){
+        var _this = this;
+        var files = files
+        if( sortby && ( sortby instanceof Array) ){
+            var duplist = []
+            log.debug("Multiple sort criteria selected!")
+            for( let current_sort of sortby){              
+                if (duplist.length == 0){
+                    duplist = await this.dupFiles(files,current_sort)
+                }
+                else{
+                    duplist = await duplist.reduce( async (prev,next)=>{
+                        var curduplist = await _this.dupFiles(next,current_sort)
+                        log.silly(curduplist)
+                        return (await prev).concat(curduplist)  
+                    }, Promise.resolve([]))
+                }
+                       
+            }
+            return duplist;
+        }
+
+        if( sortby == "hash") {
+            for(var i = 0; i < files.length ; i++){
+                if(!files[i].hash) {
+                    await files[i].hashfn()
+                }
+            }
+            log.silly("All Hashes are confirmed!")
+        }
+        function compare(a,b){
+            var aval = null
+            var bval = null
+            if(sortby == "name"){
+                aval = a.name
+                bval = b.name			
+            }else if (sortby == "size"){
+                aval = a.size
+                bval = b.size			
+
+            }else if (sortby == "ctime"){
+                aval = a.ctime.getTime()
+                bval = b.ctime.getTime()			
+
+            }else if (sortby == "mtime"){
+                aval = a.mtime.getTime()
+                bval = b.mtime.getTime()			
+
+            }else if (sortby == "hash"){
+                aval = a.hash
+                bval = b.hash			
+
+            }else{
+                throw "Invalid Sort Selection"
+            }
+
+            if( aval > bval ) {
+                return 1
+            }
+            else if ( aval < bval ) {
+                return -1
+            }
+            else {
+                return(0)
+            }
+        }
+        files.sort(compare)
+        var set = 0 ;
+        var duplist = []
+        var total = files.length
+        files.forEach((file,index,arr)=>{
+            var nextMatch = arr[index+1] && ( ! compare( arr[index] , arr[index+1]  )   )
+            var prevMatch = arr[index-1] &&  ( ! compare( arr[index] , arr[index-1]  )   )
+            if( nextMatch ){
+                //console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+                if (!duplist[set]){
+                    duplist.push([file])
+                }else{
+                    duplist[set].push(file)
+                }
+            }
+            else if( prevMatch ){
+                //console.log(index,"of",total,"Set:",set,file.mtime, file.name)
+                duplist[set].push(file)
+                set++
+            }
+        })
+        return duplist;
+    }
 }
